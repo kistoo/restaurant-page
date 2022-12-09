f47d6268eac62a406f07c0b341dcf86644b49dd2
kistoo <tomio.nakamura15@gmail.com>
2022-12-09 15:22:14 -0500

    Change home design

--- src/loadHome.js
+++ src/loadHome.js
@@ -6,27 +6,39 @@ function loadHome() {
     const content = document.getElementById('content');
     content.innerHTML = "";
 
+    //about us section
+    const section1 = document.createElement('div');
+    section1.classList = "section";
     const title = document.createElement('h1');
     title.textContent = "About us";
-    content.appendChild(title);
+    section1.appendChild(title);
     const restaurantImage = new Image();
     restaurantImage.src = restaurant;
-    content.appendChild(restaurantImage);
+    section1.appendChild(restaurantImage);
     const description = document.createElement('p');
     description.textContent = "We are a restaurant made for anime fans. In our menu you will find dishes from your favourite animes, all of them with the best quality. We offer an unique experience with a vibrant atmosphere and servicial staff. This page and its contents are all fictional.";
-    content.appendChild(description);
+    section1.appendChild(description);
+    content.appendChild(section1);
+
+    //opening hours section
+    const section2 = document.createElement('div');
+    section2.classList = "section";
     const openingHours = document.createElement('h1');
     openingHours.textContent = "Opening hours";
-    content.appendChild(openingHours);
+    section2.appendChild(openingHours);
+    const hours = document.createElement('div');
+    hours.className = 'hours';
     const mondayToFriday = document.createElement('p');
     mondayToFriday.textContent = "Monday-Friday : 12:00 ~ 20:00";
-    content.appendChild(mondayToFriday);
+    hours.appendChild(mondayToFriday);
     const saturday = document.createElement('p');
     saturday.textContent = "Saturday : 11:00 ~ 22:00";
-    content.appendChild(saturday);
+    hours.appendChild(saturday);
     const sunday = document.createElement('p');
     sunday.textContent = "Sunday : 13:00 ~ 19:00";
-    content.appendChild(sunday);
+    hours.appendChild(sunday);
+    section2.appendChild(hours);
+    content.appendChild(section2);
 }
 
 export {loadHome}
