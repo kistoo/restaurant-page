68de8ba72c2d2d826551e85f4aa6701d9b0474c8
kistoo <tomio.nakamura15@gmail.com>
2022-12-09 15:50:26 -0500

    Change image display

--- src/loadHome.js
+++ src/loadHome.js
@@ -12,8 +12,8 @@ function loadHome() {
     const title = document.createElement('h1');
     title.textContent = "About us";
     section1.appendChild(title);
-    const restaurantImage = new Image();
-    restaurantImage.src = restaurant;
+    const restaurantImage = document.createElement('div');
+    restaurantImage.id = "about-us";
     section1.appendChild(restaurantImage);
     const description = document.createElement('p');
     description.textContent = "We are a restaurant made for anime fans. In our menu you will find dishes from your favourite animes, all of them with the best quality. We offer an unique experience with a vibrant atmosphere and servicial staff. This page and its contents are all fictional.";
